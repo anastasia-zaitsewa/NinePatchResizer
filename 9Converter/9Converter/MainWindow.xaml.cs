f01c4d11af8705b5598fa1f7223775cb15e7b5db
Anastasia Zaitsewa <anastasia.zaitsewa@gmail.com>
2013-03-26 16:37:13 +0400

    Resizing for x6, x4, x3 Matrices

--- 9Converter/9Converter/MainWindow.xaml.cs
+++ 9Converter/9Converter/MainWindow.xaml.cs
@@ -31,6 +31,10 @@ namespace _9Converter
             int sourceWidth = Source.Width;
             int sourceHeight = Source.Height;
             List<int>[] frameMatrix = new List<int>[4];
+            frameMatrix[0] = new List<int>();
+            frameMatrix[1] = new List<int>();
+            frameMatrix[2] = new List<int>();
+            frameMatrix[3] = new List<int>();
 
             #region Fill frameMatrix
             //horizontal-top
@@ -45,47 +49,47 @@ namespace _9Converter
                 }
                 else
                 {
-                    frameMatrix[0].Add(count);
+                    (frameMatrix[0]).Add(count);
                     curColor = Source.GetPixel(i, 0);
-                    count = 0;
+                    count = 1;
                 }
             }
             frameMatrix[0].Add(count);
 
             //vertical-right
-            curColor = Source.GetPixel(sourceWidth, 0);
+            curColor = Source.GetPixel(sourceWidth-1, 0);
             count = 0;
 
             for (int i = 0; i < sourceHeight; i++)
             {
-                if (Source.GetPixel(sourceWidth, i) == curColor)
+                if (Source.GetPixel(sourceWidth-1, i) == curColor)
                 {
                     count++;
                 }
                 else
                 {
                     frameMatrix[1].Add(count);
-                    curColor = Source.GetPixel(sourceWidth, i);
-                    count = 0;
+                    curColor = Source.GetPixel(sourceWidth-1, i);
+                    count = 1;
                 }
             }
             frameMatrix[1].Add(count);
 
             //horizontal-bottom
-            curColor = Source.GetPixel(0, sourceHeight);
+            curColor = Source.GetPixel(0, sourceHeight-1);
             count = 0;
 
             for (int i = 0; i < sourceWidth; i++)
             {
-                if (Source.GetPixel(i, sourceHeight) == curColor)
+                if (Source.GetPixel(i, sourceHeight-1) == curColor)
                 {
                     count++;
                 }
                 else
                 {
                     frameMatrix[2].Add(count);
-                    curColor = Source.GetPixel(i, sourceHeight);
-                    count = 0;
+                    curColor = Source.GetPixel(i, sourceHeight-1);
+                    count = 1;
                 }
             }
             frameMatrix[2].Add(count);
@@ -104,24 +108,53 @@ namespace _9Converter
                 {
                     frameMatrix[3].Add(count);
                     curColor = Source.GetPixel(0, i);
-                    count = 0;
+                    count = 1;
                 }
             }
             frameMatrix[3].Add(count);
             #endregion
 
             #region Create resizingMatrix6, 4 and 3
-            List<int>[] resizingMatrix6 = new List<int>[4];
-            int k = 6;
+            List<int>[] resizingMatrix6 = Resizing(frameMatrix, 6);
+            List<int>[] resizingMatrix4 = Resizing(frameMatrix, 4);
+            List<int>[] resizingMatrix3 = Resizing(frameMatrix, 3);
+            int ch;
+            #endregion
+        }
+
+        private static List<int>[] Resizing(List<int>[] frameMatrix, int k)
+        {
+            int res;
+            List<int>[] resizingMatrix = new List<int>[4];
+            resizingMatrix[0] = new List<int>();
+            resizingMatrix[1] = new List<int>();
+            resizingMatrix[2] = new List<int>();
+            resizingMatrix[3] = new List<int>();
+
             for (int i = 0; i < frameMatrix.Length; i++)
             {
                 for (int j = 0; j < frameMatrix[i].Count; j++)
                 {
-                    //frameMatrix[i][j]
-                    //resizingMatrix6[i].Add();
+                    res = k * frameMatrix[i][j] / 8;
+
+                    if (res < 1)
+                    {
+                        resizingMatrix[i].Add(1);
+                    }
+                    else
+                    {
+                        if (k * frameMatrix[i][j] % 8 < 5)
+                        {
+                            resizingMatrix[i].Add(res);
+                        }
+                        else
+                        {
+                            resizingMatrix[i].Add(res + 1);
+                        }
+                    }
                 }
             }
-            #endregion
+            return resizingMatrix;
         }
     }
 }
